2e56f4b63a1b47af7671ebb1243d6e349c94d0ad
jfulop99 <66125323+jfulop99@users.noreply.github.com>
2021-06-18 07:49:40 +0200

    LocationsControllerTest (Unit) + .mvn

---
+++ locations-spring-solution/locations/src/test/java/locations/LocationsControllerTest.java
@@ -0,0 +1,31 @@
+package locations;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class LocationsControllerTest {
+
+    @Mock
+    LocationsService locationsService;
+
+    @InjectMocks
+    LocationsController locationsController;
+
+    @Test
+    void getLocationsTest() {
+        when(locationsService.getLocations())
+                .thenReturn(List.of(new Location("Bécs",45.497912,19.040235), new Location("Budapest",47.497912,19.040235),
+                new Location("Prága",48.497912,19.040235)));
+        assertThat(locationsController.getLocations()).endsWith("Location{id=null, name='Prága', lat=48.497912, lon=19.040235}");
+    }
+}
