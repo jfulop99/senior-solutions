ddded445b520295f934f472b11247c05e8f9dae9
jfulop99 <66125323+jfulop99@users.noreply.github.com>
2021-06-18 06:44:01 +0200

    Configuration

--- locations-spring-solution/src/main/java/locations/LocationsSpringSolutionApplication.java
+++ locations-spring-solution/src/main/java/locations/LocationsSpringSolutionApplication.java
@@ -2,6 +2,7 @@ package locations;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LocationsSpringSolutionApplication {
@@ -9,5 +10,10 @@ public class LocationsSpringSolutionApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LocationsSpringSolutionApplication.class, args);
 	}
+
+//	@Bean
+//	public LocationsService locationsService(){
+//		return new LocationsService();
+//	}
 
 }
